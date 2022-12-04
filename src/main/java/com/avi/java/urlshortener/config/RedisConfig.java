39454562418bc98e1f737799c30195f56b8d0972
Avi Singh <avi.benten@gmail.com>
2022-12-04 22:55:16 +0530

    Final URL shortner assignmet

---
+++ src/main/java/com/avi/java/urlshortener/config/RedisConfig.java
@@ -0,0 +1,34 @@
+package com.avi.java.urlshortener.config;
+
+import com.avi.java.urlshortener.models.Url;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+
+  @Autowired
+  ObjectMapper mapper;
+
+  @Autowired
+  RedisConnectionFactory connectionFactory;
+
+  @Bean
+  RedisTemplate<String, Url> redisTemplate() {
+    final RedisTemplate<String, Url> redisTemplate = new RedisTemplate<>();
+    Jackson2JsonRedisSerializer valueSerializer = new Jackson2JsonRedisSerializer(Url.class);
+    valueSerializer.setObjectMapper(mapper);
+    redisTemplate.setConnectionFactory(connectionFactory);
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setValueSerializer(valueSerializer);
+    return redisTemplate;
+  }
+}
